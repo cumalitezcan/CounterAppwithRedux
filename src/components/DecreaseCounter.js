67aca91f0a8a9b25dabd0ea0cbf03762c99d921a
cumalitezcan <cumalitezcan@gmail.com>
2020-11-09 18:29:27 +0300

    last commit

--- src/components/DecreaseCounter.js
+++ src/components/DecreaseCounter.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { decreaseCounter } from '../redux/actions/counterActions';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default class DecreaseCounter extends Component {
+class DecreaseCounter extends Component {
     render() {
         return (
             <div>
-                
+                <button onClick={e => { this.props.dispatch(decreaseCounter()); }} >
+                1 Eksilt
+                </button>
             </div>
         )
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return {actions:bindActionCreators(decreaseCounter,dispatch)}
+}
+
+export default connect(mapDispatchToProps)(DecreaseCounter);
